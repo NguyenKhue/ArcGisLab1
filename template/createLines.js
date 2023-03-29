087f1f0232435fd394b3ab7449d2ef81d2138a4c
Nguyen Truc Lan <ngtruclan2302@gmail.com>
2023-03-29 22:27:22 +0700

    Quang Nam, Da Nang, Quang Ngai, Binh Dinh (50%)

--- template/createLines.js
+++ template/createLines.js
@@ -1,16 +1,16 @@
-import { point_template_area } from './pointTemplate.js';
+import { point_template_area } from "./pointTemplate.js";
 
-export function createLines(paths,  name, location, color = [226, 119, 40]) {
-    return {
-        type: "polyline",
-        paths: paths,
-        symbol: {
-            type: "simple-line",
-            color: [226, 119, 40], // orange
-            width: 2
-        },
-        Name: "Làng đại học",
-        Location: "Quận Thủ Đức",
-        popupTemplate: point_template_area
-    }
+export function createLines(paths, name, location, color = [226, 119, 40]) {
+  return {
+    type: "polyline",
+    paths: paths,
+    symbol: {
+      type: "simple-line",
+      color: color, // orange
+      width: 2,
+    },
+    Name: name,
+    Location: location,
+    popupTemplate: point_template_area,
+  };
 }
